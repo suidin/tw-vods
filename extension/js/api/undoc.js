027170f4e14383908c113085bcf18464aa172d22
suidin <snmdnll@protonmail.com>
2018-12-16 19:03:48 +0100

    fix api bug

--- extension/js/api/undoc.js
+++ extension/js/api/undoc.js
@@ -39,7 +39,7 @@ class UndocumentedApi extends AbstractApi{
             let sig = json.sig;
             let token = json.token;
             let p = parseInt(Math.random() * 999999);
-            let url = `https://usher.ttvnw.net/api/channel/hls/${this.vid.toLowerCase()}.m3u8?player=twitchweb&p=${p}&type=any&allow_source=true&allow_audio_only=true&allow_spectre=false&nauthsig=${sig}&nauth=${token}`;
+            let url = `https://usher.ttvnw.net/api/channel/hls/${sId.toLowerCase()}.m3u8?player=twitchweb&p=${p}&type=any&allow_source=true&allow_audio_only=true&allow_spectre=false&nauthsig=${sig}&nauth=${token}`;
             return url;
         });
     }
